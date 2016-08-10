ba5f7a1e47aa947dc412bfe4ea3af1dfde67ac67
payne-chris-r <06cpayne@gmail.com>
2016-08-09 21:30:22 -0400

    Moves Character creation from examples.rb to seeds
    This data shouldn't have been in examples at all. It's seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Character.create(
+  [
+    {
+      name: 'Phantom Assassin',
+      base_stat: 'Agility',
+      ranged: false
+    },
+    {
+      name: 'Sven',
+      base_stat: 'Strength',
+      ranged: false
+    },
+    {
+      name: 'Centaur',
+      base_stat: 'Strength',
+      ranged: false
+    },
+    {
+      name: 'Anti Mage',
+      base_stat: 'Agility',
+      ranged: false
+    },
+    {
+      name: 'Medusa',
+      base_stat: 'Agility',
+      ranged: true
+    },
+    {
+      name: 'Dazzle',
+      base_stat: 'Intelligence',
+      ranged: true
+    },
+    {
+      name: 'Disruptor',
+      base_stat: 'Intelligence',
+      ranged: true
+    },
+    {
+      name: 'Lone Druid',
+      base_stat: 'Agility',
+      ranged: false
+    },
+    {
+      name: 'Undying',
+      base_stat: 'Strength',
+      ranged: false
+    },
+    {
+      name: 'Omniknight',
+      base_stat: 'Intelligence',
+      ranged: false
+    },
+    {
+      name: 'Slawwdawww',
+      base_stat: 'Strength',
+      ranged: false
+    }
+  ]
+)
