58d588357655f64d43c8be2fbc88da2e662e742e
payne-chris-r <06cpayne@gmail.com>
2016-09-09 15:28:22 -0400

    Update scripts

--- scripts/show-profile.sh
+++ scripts/show-profile.sh
@@ -3,12 +3,12 @@ curl --include --request GET http://localhost:3000/profiles/1 \
   --header "Content-Type: application/json"
 
 curl --include --request POST http://localhost:3000/profiles \
-  --header "Authorization: Token token=BAhJIiUwOWI2NTFmZWQyYjc0ZWFiNGM1NzM0MmY4OTU0OGM2OAY6BkVG--a9a081dd7d2d0c4881d829703054840f6cb3ad51" \
+  --header "Authorization: Token token=BAhJIiUzYjk0ZjgwMzQxMzI4OWMzNmY4MmEwMDMzZjQzMjY3MwY6BkVG--9e72b7af0e27cf110798506aeb15e498c211a34d" \
   --header "Content-Type: application/json" \
   --data '{
     "profile": {
-      "first_name": "Steve",
-      "last_name": "Brian",
+      "first_name": "Luke",
+      "last_name": "Costanza",
       "nationality": "USA"
     }
   }'
