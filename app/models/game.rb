0300e8cad92d643c0c4a676d480dc5a6a517f299
payne-chris-r <06cpayne@gmail.com>
2016-09-29 22:37:44 -0400

    Add has_many through to profile and game
    Add the join table relationship with has_many through :players for
    both profile and game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
   has_many :players, inverse_of: :game
+  has_many :profiles, through: :players
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,6 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
   has_many :players, inverse_of: :profile
+  has_many :games, through: :players
   validates :user, uniqueness: true
 end
