94e29b1bdcb30e4ca5af999c63329d7506507d9b
payne-chris-r <06cpayne@gmail.com>
2016-09-29 22:40:10 -0400

    Add full? method to game model
    game.full? should return true if a game has 4 players in it already
    `true if game.players.length > 4`

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,13 @@
 class Game < ActiveRecord::Base
   has_many :players, inverse_of: :game
   has_many :profiles, through: :players
+
+  def full?
+    p "players.length is", players.length
+    if players.length >= 4
+      true
+    else
+      false
+    end
+  end
 end
