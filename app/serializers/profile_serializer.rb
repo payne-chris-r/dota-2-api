e2b0dbaba99b93fb06fbf9e0f6feb9c6cc869b57
payne-chris-r <06cpayne@gmail.com>
2016-10-04 18:53:56 -0400

    Add line for formatting

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,5 +1,6 @@
 class ProfileSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :nationality, :ranking, :players, :user
+  attributes :id, :first_name, :last_name, :nationality, :ranking,
+             :players, :user
 
   def players
     object.players.pluck(:id)
