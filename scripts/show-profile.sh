0acc1623f85f02b8c97ca1d5f2afc58b308ce36a
payne-chris-r <06cpayne@gmail.com>
2016-10-29 00:21:15 -0400

    Add script for previous commit

--- scripts/show-profile.sh
+++ scripts/show-profile.sh
@@ -1,25 +1,30 @@
-curl --include --request GET http://localhost:3000/profiles/1 \
-  --header "Authorization: Token token=BAhJIiU2NzBmMTU3NGJhOTAyOGVhZjgyYTlmMTU2MGYxNWRkMAY6BkVG--6f39b1207b686401b9387643d5abaeac9e9e5268" \
+curl --include --request GET http://localhost:3000/profiles/5 \
+  --header "Authorization: Token token=BAhJIiUwNWNjNGU2MWI4OWFmOWJmYjE4ZWJiOGI2NjlkMzNlNAY6BkVG" \
   --header "Content-Type: application/json"
 
 curl --include --request POST http://localhost:3000/profiles \
-  --header "Authorization: Token token=BAhJIiUzYjk0ZjgwMzQxMzI4OWMzNmY4MmEwMDMzZjQzMjY3MwY6BkVG--9e72b7af0e27cf110798506aeb15e498c211a34d" \
+  --header "Authorization: Token token=BAhJIiVjMGM4YzU0N2VmMGM5MWZkMzUyYmFhMWRkMzEzMmVjNwY6BkVG--bf44a69b7a105d684ab3d335b5fa5efaef5c5234" \
   --header "Content-Type: application/json" \
   --data '{
     "profile": {
-      "first_name": "Luke",
-      "last_name": "Costanza",
-      "nationality": "USA"
+      "first_name": "B Reezy",
+      "last_name": "My Neezy",
+      "nationality": "USA",
+      "abandons": 23,
+      "reports": 12,
+      "language": "English",
+      "carry":"true",
+      "support":"true",
+      "offlane":"false"
     }
   }'
 
-  curl --include --request POST http://localhost:3000/profiles \
-    --header "Authorization: Token token=BAhJIiVjYWQ3NTgzY2QxMDljZmNiYjM2NWNmZjViZDA0ZWJjYwY6BkVG--2008518766e17fd637998de616d92bd7db4c5443" \
-    --header "Content-Type: application/json" \
-    --data '{
-      "profile": {
-        "first_name": "Bobby",
-        "last_name": "Nicko",
-        "nationality": "USA"
-      }
-    }'
+curl --include --request PATCH http://localhost:3000/profiles/5 \
+  --header "Authorization: Token token=BAhJIiUwNmU2NzUxMTVhNzNhNmI2MWYwY2RiNTY4MmFmZTY4YgY6BkVG--c57d81cba218b7262d60a194ecdff44fb57a1c1c" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "profile": {
+      "user_id": "6",
+      "positions": ["carry","support"]
+    }
+  }'
